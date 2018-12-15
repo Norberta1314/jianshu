78ddf157321c06cfa8f2a1eef2bf988e4bd209c3
norberta <norbertahp@163.com>
2018-12-15 16:26:41 +0800

    search active

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import {
     HeaderWrapper,
     Logo,
@@ -7,35 +8,68 @@ import {
     NavSearch,
     Addition,
     Button,
-    SearchWripper
+    SearchWrapper
 } from './style'
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            focused: false,
+        }
+        this.handleInputFocus = this.handleInputFocus.bind(this);
+        this.handleInputBlur = this.handleInputBlur.bind(this);
+    }
+
     render() {
         return (
             <HeaderWrapper>
-                <Logo />
+                <Logo/>
                 <Nav>
-                    <NavItem className = 'left active'>首页</NavItem>
-                    <NavItem className = 'left'>下载app</NavItem>
-                    <NavItem className = 'right'>登陆</NavItem>
-                    <NavItem className = 'right'>
+                    <NavItem className='left active'>首页</NavItem>
+                    <NavItem className='left'>下载app</NavItem>
+                    <NavItem className='right'>登陆</NavItem>
+                    <NavItem className='right'>
                         <i className='iconfont'> &#xe636;</i>
                     </NavItem>
-                    <SearchWripper>
-                        <NavSearch></NavSearch>
-                        <i className='iconfont'> &#xe623;</i>
-                    </SearchWripper>
+                    <SearchWrapper>
+                        <CSSTransition
+                            in={this.state.focused}
+                            timeout={200}
+                            classNames="slide"
+                        >
+                            <NavSearch
+                                className={ this.state.focused ? 'focused' : '' }
+                                onFocus={ this.handleInputFocus }
+                                onBlur={ this.handleInputBlur }
+                            />
+                        </CSSTransition>
+                        <i className={ this.state.focused ? 'focused iconfont' : 'iconfont' }> &#xe623;</i>
+                    </SearchWrapper>
                     <Addition>
-                        <Button className = 'writting'>
+                        <Button className='writting'>
                             <i className='iconfont'> &#xe617;</i>
                             写文章
                         </Button>
-                        <Button className = 'reg'>注册</Button>
+                        <Button className='reg'>注册</Button>
                     </Addition>
                 </Nav>
             </HeaderWrapper>
         );
+    }
+
+    handleInputFocus() {
+        // console.log("111");
+        this.setState({
+            focused: true
+        })
+    }
+
+    handleInputBlur() {
+        this.setState({
+            focused: false
+        })
     }
 }
 
